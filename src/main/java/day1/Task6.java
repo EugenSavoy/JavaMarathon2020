11fb6b6b6316e7d2adf5f8474474376ec24cf341
EugenSavoy <76279734+EugenSavoy@users.noreply.github.com>
2020-12-24 19:42:03 +0300

    day 1

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 3;
+        for (int i = 1; i <10; i++) {
+            int result = k * i;
+            System.out.printf("%s x %s = %s \n", i, k, result);
+        }
     }
 }
