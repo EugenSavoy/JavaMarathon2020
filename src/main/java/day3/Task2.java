8a1d610ae5dda1c098ad0d7f937995c6d6b81dcd
EugenSavoy <76279734+EugenSavoy@users.noreply.github.com>
2020-12-26 12:28:45 +0300

    day 3

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,25 @@
 package day3;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            double divident = sc.nextDouble();
+            double divider = sc.nextDouble();
+
+            if (divider == 0) {
+                break;
+            }
+
+            System.out.println(divident/divider);
+
+        }
+        sc.close();
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,24 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        for (int i = 0; i <5; i++) {
+
+            double divident = sc.nextDouble();
+            double divider = sc.nextDouble();
+
+            if (divider == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+
+            System.out.println(divident/divider);
+
+        }
+        sc.close();
     }
 }
