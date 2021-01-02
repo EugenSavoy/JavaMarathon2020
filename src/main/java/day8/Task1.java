a93b62310082fffc527452db0da8e54c64204648
EugenSavoy <76279734+EugenSavoy@users.noreply.github.com>
2021-01-02 22:39:52 +0300

    day 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,14 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        long before = System.currentTimeMillis();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 20000; i++) {
+            stringBuilder.append(i);
+            stringBuilder.append(" ");
+        }
+        long after = System.currentTimeMillis();
+        System.out.println(stringBuilder);
+        System.out.println(after - before);
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,13 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        long before = System.currentTimeMillis();
+        String string = "";
+        for (int i = 0; i < 20000; i++) {
+            string = string + i +" ";
+        }
+        long after = System.currentTimeMillis();
+        System.out.println(string);
+        System.out.println(after - before);
     }
 }
